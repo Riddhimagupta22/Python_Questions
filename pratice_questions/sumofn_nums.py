25858e2a0a974f82eb873e6b610612334d4e5cb4
Riddhima Gupta <griddhima226@gmail.com>
2025-01-14 15:36:09 +0530

    add file

---
+++ pratice_questions/sumofn_nums.py
@@ -0,0 +1,8 @@
+num = int(input("Enter the value : \n"))
+
+if(num >= 0):
+    print("Sum of number : ")
+else:
+    print("Please! Enter a positive number.\n")
+
+print("Thank You!! Have a nice Day.")
